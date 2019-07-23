af9d53c56a0328450a07cf73b33f5712f1f92e80
Guilherme Garcia <guilhermegs@ciandt.com>
2019-07-22 23:01:06 -0300

    Classe inicial para inicio do sistema

---
+++ src/main/java/com/java/estudos/java/Sistema.java
@@ -0,0 +1,35 @@
+package com.java.estudos.java;
+
+public class Sistema {
+
+	public int start() {
+		// TODO implementação pendente
+		
+		// aqui inicia o sistema, exibe um menu
+		// a partir da opção escolhida no menu, chama uma funcionalidade
+		
+		// sugestão: sistema de escola com opções:
+		// cadastrar aluno
+		// alterar aluno
+		// excluir aluno
+		// listar alunos
+		
+		// cadastrar aluno supletivo
+		// alterar aluno supletivo
+		// excluir aluno supletivo
+		// listar alunos supletivo
+		
+		// cadastrar professor
+		// alterar professor
+		// excluir professor
+		// listar professores
+		
+		// cadastrar funcionário
+		// alterar funcionário
+		// excluir funcionário
+		// listar funcionários
+		
+		return 0;
+	}
+	
+}
